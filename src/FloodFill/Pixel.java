09027fde294f71808a9dad83816464fed7182562
luis <luotasss@gmail.com>
2024-09-22 22:05:52 -0300

    floodfill com queue :D

---
+++ src/FloodFill/Pixel.java
@@ -0,0 +1,34 @@
+package FloodFill;
+
+public class Pixel {
+    private final int x;
+    private final int y;
+    private int rgb;
+
+    public Pixel(int x, int y, int cor) {
+        this.x = x;
+        this.y = y;
+        this.rgb = cor;
+    }
+
+    public Pixel(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int getRgb() {
+        return rgb;
+    }
+
+//    public String getStingCordenadas() {
+//        return "(" + x + "," + y + ")";
+//    }
+}
